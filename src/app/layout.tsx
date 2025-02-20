4be05ae26acf37d241e7b7c7edc2f59ceff1e4d3
luizamc003 <luizamachadocn@outlook.com>
2025-02-20 18:52:32 -0300

    add footer inicial

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,18 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/footer";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+  style: "normal",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Desafio Next.JS 2024.2",
-  description: "Desafio Next.JS, Typescript, Tailinwd, Prisma, semestre 2024.2 para todos os membros da Code JR",
+  description:
+    "Desafio Next.JS, Typescript, Tailinwd, Prisma, semestre 2024.2 para todos os membros da Code JR",
 };
 
 export default function RootLayout({
@@ -15,8 +21,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt">
+      <body className={inter.className}>
+        {children}
+
+        <Footer />
+      </body>
     </html>
   );
 }
---
+++ src/components/footer/index.tsx
@@ -0,0 +1,94 @@
+import Image from "next/image";
+
+export default function Footer() {
+  return (
+    <div className="flex flex-col w-full lg:justify-center border-t-4 border-secondary-pink  text-gray text-base text-center lg:text-start">
+      <div className="flex justify-between items-start w-full px-8 xl:px-16 py-1">
+        <div className="w-full flex flex-col lg:flex-row lg:justify-between py-5 m-auto lg:gap-10">
+          <div className="flex w-full flex-col pb-10 lg:pb-0 flex-1">
+            <h1 className="font-semibold pb-4 text-dark">SOCIAL</h1>
+            <div className="flex flex-col gap-2">
+              <a href="">Facebook</a>
+              <a href="">Instagram</a>
+              <a href="">Twitter</a>
+            </div>
+            <div className="flex w-full flex-col items-center lg:items-start pb-5 lg:pb-0">
+              <h1 className="font-semibold pb-4 mt-4 text-dark">
+                FORMAS DE PAGAMENTO
+              </h1>
+              <Image
+                src="https://lowell.fbitsstatic.net/sf/img/footer/pagamento-2.svg?theme=main&v=202502171649"
+                alt="pagamento"
+                loading="lazy"
+                width={202}
+                height={56}
+                className="w-[202px]"
+              />
+            </div>
+          </div>
+
+          <div className="flex justify-center items-center ">
+            <Image
+              src="/logo/beleza-express-logo.png"
+              alt="logo"
+              width={500}
+              height={300}
+            />
+          </div>
+
+          <div className="flex w-full flex-col pb-10 lg:pb-0 flex-1">
+            <div className="flex lg:items-end flex-col">
+              <h1 className="font-semibold pb-4 text-dark">
+                ATENDIMENTO AO CLIENTE
+              </h1>
+              <div className="flex flex-col flex-end gap-2 mb-4">
+                <p>email@hotmail.com</p>
+                <p>(32) 9150-1320</p>
+                <p>Fale Conosco</p>
+                <p>
+                  R. Duque de Caxias, 115 <br /> Poço Rico, Juiz de Fora - MG{" "}
+                  <br />
+                  36020-280, Brasil
+                </p>
+              </div>
+              <div className="flex flex-col justify-center gap-4 pb-5 lg:pb-0 lg:border-none md:justify-center ">
+                <p className="font-semibold text-dark">SEGURANÇA</p>
+                <div className="flex gap-2 lg:gap-5 item">
+                  <Image
+                    src="https://lowell.fbitsstatic.net/sf/img/footer/ssl.svg?theme=main&v=202502171649"
+                    alt="ssl"
+                    width={47}
+                    height={32}
+                    className="w-[80px]"
+                  />
+                  <Image
+                    src="/footer/google_site_seguro.png"
+                    alt="google"
+                    width={357}
+                    height={141}
+                    className="h-[56px] w-auto"
+                  />
+                  <Image
+                    src="https://lowell.fbitsstatic.net/sf/img/footer/seguranca.svg?theme=main&v=202502171649"
+                    alt="seguranca"
+                    width={55}
+                    height={25}
+                    className="w-[80px]"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full">
+        <div className="flex justify-center items-center lg:flex-row py-1 bg-secondary-pink">
+          <p className="text-center order-3 lg:order-1 text-danger w-full">
+            © Beleza Express - Todos direitos reservados
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
