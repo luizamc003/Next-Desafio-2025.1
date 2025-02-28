cbfb073590c2b449ba0ea2a6468b682f3bf14d0b
luizamc003 <luizamachadocn@outlook.com>
2025-02-28 09:25:04 -0300

    fix missio container

--- src/components/values-mission-containers/index.tsx
+++ src/components/values-mission-containers/index.tsx
@@ -1,6 +1,24 @@
 export default function ValuesContainer() {
   return (
-    <div className="flex justify-center items-center p-4">
+    <div className="flex justify-center items-center flex-col gap-10 md:flex-row">
+      <div className="flex flex-col justify-center bg-[#f49cbb63] p-4 rounded-[50px] gap-4 shadow-lg w-80">
+        <span className="text-2xl mb-2 text-center">Missão</span>
+        <span className="text-center">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras erat
+          nisi, aliquet a nisi nec, ultricies ultrices lacus. Maecenas sagittis
+          sem nec laoreet hendrerit. Cras porta viverra hendrerit. Fusce
+          ultrices tincidunt dolor sit amet tempo
+        </span>
+      </div>
+      <div className="flex flex-col justify-center bg-[#f49cbb63] p-4 rounded-[50px] gap-4 shadow-lg w-80">
+        <span className="text-2xl mb-2 text-center">Missão</span>
+        <span className="text-center">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras erat
+          nisi, aliquet a nisi nec, ultricies ultrices lacus. Maecenas sagittis
+          sem nec laoreet hendrerit. Cras porta viverra hendrerit. Fusce
+          ultrices tincidunt dolor sit amet tempo
+        </span>
+      </div>
       <div className="flex flex-col justify-center bg-[#f49cbb63] p-4 rounded-[50px] gap-4 shadow-lg w-80">
         <span className="text-2xl mb-2 text-center">Missão</span>
         <span className="text-center">
